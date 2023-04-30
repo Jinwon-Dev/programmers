a7ec1c23a9135832a668e8e5c9f712d2077f7606
Jinwon-Dev <yjw9424@naver.com>
2023-05-01 00:02:08 +0900

    프로그래머스 : [해시 - 완주하지 못한 선수]

---
+++ src/hash/participant/Solution.java
@@ -0,0 +1,24 @@
+package hash.participant;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static String solution(String[] participant, String[] completion) {
+
+        String answer = "";
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i;
+        for (i = 0; i < completion.length; i++) {
+
+            if (!participant[i].equals(completion[i])) return participant[i];
+        }
+
+        answer = participant[i];
+
+        return answer;
+    }
+}
