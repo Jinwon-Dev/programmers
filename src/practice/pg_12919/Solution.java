e50b8fbd00f9850dc0091b3bebe755a612c77208
Jinwon-Dev <yjw9424@naver.com>
2023-07-28 00:06:01 +0900

    프로그래머스 : [연습 문제 - 서울에서 김서방 찾기]

---
+++ src/practice/pg_12919/Solution.java
@@ -0,0 +1,18 @@
+package practice.pg_12919;
+
+class Solution {
+    public String solution(String[] seoul) {
+
+        String answer = "";
+
+        for (int i = 0; i < seoul.length; i++) {
+
+            if (seoul[i].equals("kim")) {
+                answer = "김서방은 " + i + "에 있다";
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
